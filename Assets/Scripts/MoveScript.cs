6eae9e51736db73bb860745466f14f7d92098a42
Georgiy <vladmagdiuk@gmail.com>
2022-08-16 14:49:07 +0300

    Initial commit

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+
+    public float speed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject spawnItem;
+    public float frequency;
+
+    float lastSpawnedTime;
+
+    void Start()
+    {
+      
+    }
+
+    void Update()
+    {
+        if(Time.time > lastSpawnedTime + frequency)
+        {
+            Spawn();
+            lastSpawnedTime = Time.time;
+            frequency = Random.Range(1, 6);
+        }
+    }
+
+    public void Spawn()
+    {
+        GameObject newSpawnedObject = Instantiate(spawnItem, transform.position, Quaternion.Euler(0,90,0));
+        newSpawnedObject.transform.parent = transform;
+    }
+}
