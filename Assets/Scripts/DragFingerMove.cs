24df3f654748f2928e4dda5b30a024047d74b4a2
Georgiy <vladmagdiuk@gmail.com>
2022-08-24 20:37:39 +0300

    Added logic for Sight

--- Assets/Scripts/DragFingerMove.cs
+++ Assets/Scripts/DragFingerMove.cs
@@ -1,33 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class DragFingerMove : MonoBehaviour
 {
-    private Vector3 touchPosition;
-    private Rigidbody2D rb;
-    private Vector3 direction;
-    private float moveSpeed = 10f;
+  private Vector3 touchPosition;
+  private Rigidbody2D rb;
+  private Vector3 direction;
+  private float moveSpeed = 1f;
 
-    private void Start()
+  private void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.touchCount > 0)
     {
-        rb = GetComponent<Rigidbody2D>();
+      Touch touch = Input.GetTouch(0);
+      Vector3 direction = touch.position - rb.position;
+      rb.transform.Translate(direction * Time.deltaTime * moveSpeed);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.touchCount > 0)
-        {
-            //Touch touch = Input.GetTouch(0);
-            //touchPosition = Camera.main.ScreenToWorldPoint(touchPosition);
-            //touchPosition.z = 0;
-            //direction = (touchPosition = transform.position);
-            //rb.velocity = new Vector2(direction.x, direction.y) * moveSpeed;
-
-            //if (touch.phase == TouchPhase.Ended)
-            //    rb.velocity = Vector2.zero;
-            Debug.Log("TEST");
-        }
-    }
+  }
 }
